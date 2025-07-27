d94f30fdfb626c4aadc626359ed1caab82053df5
surajit7332 <surajitsingh711@gmail.com>
2025-07-27 05:17:09 -0700

    Create remove.py

---
+++ Safegram/modules/remove.py
@@ -0,0 +1,10 @@
+import logging
+from pyrogram.enums import ChatMemberStatus
+from pyrogram.types import ChatMemberUpdated
+
+from config import LOGGER_ID
+from Safegram import Safegram
+from Safegram.mongo.chatsdb import add_chat
+
+logger = logging.getLogger(__name__)
+
